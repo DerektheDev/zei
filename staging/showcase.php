58de3fc0cc69848d87eeb4d47e96b722eb1d36a9
dwysefisher <wysefisher@gmail.com>
2013-11-21 23:51:11 -0600

    created showcase.php, added content

---
+++ staging/showcase.php
@@ -0,0 +1,110 @@
+<?php include('header.php'); ?>
+<div class="container-fluid">
+
+<div class="span1">
+&nbsp;
+</div>
+
+<article class="row-fluid">
+	<header id="content_area" class="span10">
+		<div class="padded">
+			<h2>Showcase</h2>
+			<p class="body-text">Zeller Electric provides electrical design, installation, and maintenance for just about every industry. Our solutions fit all job sizes—and all budgets, too. For more information about our projects or services, or to receive a quote, please contact us. </p>
+		</div>
+	</header>
+</article>
+
+<article class="row-fluid">
+<div class="span1">
+&nbsp;
+</div>
+	<header id="content_area" class="span10">
+		<div class="padded">
+			<h3>Showcase - Commercial</h3>
+			<p class="body-text">We are Central Illinois’ pre-eminent electrical contractor for outfitting commercial-grade buildings. Whether you need interior electrical design, exterior lighting, security systems, or generator installation, Zeller Electric is always ready to meet your needs&mdash;and exceed your expectations. 
+			</p>
+		</div>
+	</header>
+
+
+	<section class="showcase_entry">
+		<div class="padded">
+			<div class="row-fluid">
+				<div class="showcase_entry_img span3">
+					<img src="<?php echo $root; ?>/images/heartland.jpg" />
+				</div>
+				<div class="showcase_entry_text span9">
+					<h4>Heartland Community College (Phase II) – Normal, IL</h4>
+					<h5>Contract: $6,700,000</h5><br />
+					<p>This project consists of additions to the Student Center and Instructional Commons, a new Child Development Lab, Fitness and Recreation Center, Community Education Center, and Receiving and Storage building, as well as site work. The project was completed in 2010.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="showcase_entry">
+		<div class="padded">
+			<div class="row-fluid">
+				<div class="showcase_entry_img span3">
+					<img src="<?php echo $root; ?>images/GraceChurch.jpg" />
+				</div>
+				<div class="showcase_entry_text span9">
+				<h4>Grace Church – Morton, IL</h4>
+				<h5>Contract: $789,000</h5><br />
+				<p>We provided the electrical work for the construction of the new church which included a new sanctuary, lobby, multiple classrooms, conference rooms, and site work.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="showcase_entry">
+		<div class="padded">
+			<div class="row-fluid">
+				<div class="showcase_entry_img span3">
+				</div>
+				<div class="showcase_entry_text span9">
+				<h4>Additional job listings</h4>
+				<h5>Gourmet Express – Gridley, IL</h5>
+				<h5>ISU Alumni Center – Normal, IL</h5>
+				<h5>Peoria Air National Guard – Peoria, IL</h5>
+				<h5>The Manna Group – Gridley, IL</h5>
+				<h5>WTVP – Peoria, IL</h5>
+				</div>
+			</div>
+		</div>
+	</section>
+</article><div class="span1">
+&nbsp;
+</div>
+
+<div class="span1">
+&nbsp;
+</div>
+
+<article id="content">
+	<header id="content_area" class="span10">
+		<div class="padded">
+			<h2>Showcase - Residential</h2>
+		</div>
+	</header>
+</article>
+
+	<section class="showcase_entry">
+		<div class="padded">
+			<div class="row-fluid">
+				<div class="showcase_entry_img span3">
+					<img src="<?php echo $root; ?>/images/175px_placeholder.png" />
+				</div>
+				<div class="showcase_entry_text span9">
+					<h4>Coming Soon!</h4>
+					<h5></h5><br />
+					<p></p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+<div class="span1">
+&nbsp;
+</div>
+</div>
+
+<?php include('footer.php'); ?>
