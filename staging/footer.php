ceaae9f82115661edb262fd4f9cec367d4ff7081
dwysefisher <wysefisher@gmail.com>
2013-11-23 22:46:24 -0600

    updated <li> for About Us and Industrial pages

--- staging/footer.php
+++ staging/footer.php
@@ -5,13 +5,14 @@
   <nav id="footer-menu" class="visible-desktop">
     <ul>
       <li><a href="<?php echo $root; ?>/">Home</a></li>
-      <li><a href="<?php echo $root; ?>/services.php">Services</a></li>
+      <li><a href="<?php echo $root; ?>/about.php">About Us</a></li>
       <li><a href="<?php echo $root; ?>/affiliations.php">Affiliations</a></li>
       <li><a href="<?php echo $root; ?>/contact.php">Contact</a></li>
     </ul>
     <ul>
       <li><a href="<?php echo $root; ?>/showcase/commercial.php">Commercial</a></li>
       <li><a href="#">Residential</a></li>
+      <li><a href="<?php echo $root; ?>/showcase/industrial.php">Industrial</a></li>
       <li><a href="<?php echo $root; ?>/showcase/agricultural.php">Agricultural</a></li>
       <li><a href="<?php echo $root; ?>/showcase/design-build.php">Design/Build</a></li>
       <li><a href="#">Data Communication</a></li>
