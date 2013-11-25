5c06cbfd3cc2c341c11073ef81d1dbeed8393fd1
dwysefisher <wysefisher@gmail.com>
2013-11-24 22:47:37 -0600

    affiliation supplier logos added

--- staging/affiliations.php
+++ staging/affiliations.php
@@ -67,17 +67,17 @@
       <h2 class="hidden-desktop visible-phone">Supplier and <br />Manufacturer Links</h2>
       <div id="lists" class="row-fluid">
         <ul class="span4">
-          <li><a href="http://www.wesco.com/" target="_blank">WESCO</a></li>
-          <li><a href="http://www.kirbyrisk.com/" target="_blank">Kirby-Risk</a></li>
-          <li><a href="http://www.lithonia.com/" target="_blank">Lithonia</a></li>
+          <li><a href="http://www.wesco.com/" target="_blank"><img src="images/wesco-logo.jpg" ALT="WESCO"></a></li>
+          <li><a href="http://www.kirbyrisk.com/" target="_blank"><img src="images/kirby-risk-logo.jpg" ALT="Kirby-Risk"></a></li>
+          <li><a href="http://www.lithonia.com/" target="_blank"><img src="images/lithonia_lighting.jpg" ALT="Lithonia Lighting"></a></li>
         </ul>
         <ul class="span4">
-          <li><a href="http://www.schneider-electric.com/site/home/index.cfm/us/" target="_blank">Square D</a></li>
-          <li><a href="http://www.cesco.com/" target="_blank">Crescent Electric</a></li>
+          <li><a href="http://www.schneider-electric.com/site/home/index.cfm/us/" target="_blank"><img src="images/schneider_electric-logo.jpg" ALT="Schneider Electric"></a></li>
+          <li><a href="http://www.cesco.com/" target="_blank"><img src="images/crescent-logo.jpg" ALT="Crescent Supply"></a></li>
         </ul>
         <ul class="span4">
-          <li><a href="http://www.graybar.com/" target="_blank">Graybar</a></li>
-          <li><a href="http://www.springfieldelectric.com/" target="_blank">Springfield Electric</a></li>
+          <li><a href="http://www.graybar.com/" target="_blank"><img src="images/graybar-logo.jpg" ALT="Graybar"></a></li>
+          <li><a href="http://www.springfieldelectric.com/" target="_blank"><img src="images/sesclogo.jpg" ALT="Springfield ELectric"></a></li>
         </ul>
       </div>
     </section>
