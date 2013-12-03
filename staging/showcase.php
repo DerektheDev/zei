4d0f4955a045b57eb5844b70bf9074b62a4a740f
dwysefisher <wysefisher@gmail.com>
2013-12-02 22:50:52 -0600

    adjusted offsets

--- staging/showcase.php
+++ staging/showcase.php
@@ -1,29 +1,20 @@
 <?php include('header.php'); ?>
 <div class="container-fluid">
 
-<div class="span1">
-&nbsp;
-</div>
 <section>
 
 	<article class="row-fluid">
-		<header id="content_area" class="span10">
+		<header id="content_area" class="span11 offset1">
 			<div class="padded">
 				<h2>Showcase</h2>
 				<p class="body-text">Zeller Electric provides electrical design, installation, and maintenance for just about every industry. Our solutions fit all job sizes—and all budgets, too. For more information about our projects or services, or to receive a quote, please contact us. </p>
 			</div>
 		</header>
 	</article>
-	<div class="span1">
-	&nbsp;
-	</div>
 </section>
 
-<div class="span1">
-&nbsp;
-</div>
 <section class="row-fluid"> <!--Commercial-->
-	<header id="content_area" class="span10">
+	<header id="content_area" class="span11 offset1">
 		<div class="padded">
 			<h3>Showcase - Commercial</h3>
 			<p class="body-text">We are Central Illinois’ pre-eminent electrical contractor for outfitting commercial-grade buildings. Whether you need interior electrical design, exterior lighting, security systems, or generator installation, Zeller Electric is always ready to meet your needs&mdash;and exceed your expectations. 
@@ -35,12 +26,10 @@
 		<div class="padded">
 
 			<div class="row-fluid">
-	<div class="span1">
-	&nbsp;
-	</div>			<div class="showcase_entry_img span3">
+		<div class="showcase_entry_img span3 offset1">
 					<img src="<?php echo $root; ?>/images/heartland.jpg" />
 				</div>
-				<div class="showcase_entry_text span7">
+				<div class="showcase_entry_text span8">
 					<h4>Heartland Community College (Phase II) – Normal, IL</h4>
 					<h5>Contract: $6,700,000</h5><br />
 					<p>This project included additions to the student center and instructional commons, new child development lab, fitness and recreation center, community education center, receiving and storage building, and site work. </p>
@@ -55,10 +44,10 @@
 	<article class="showcase_entry">
 		<div class="padded">
 			<div class="row-fluid">
-				<div class="showcase_entry_img span3">
+				<div class="showcase_entry_img span3 offset1">
 					<img src="<?php echo $root; ?>images/GraceChurch.jpg" />
 				</div>
-				<div class="showcase_entry_text span9">
+				<div class="showcase_entry_text span8">
 				<h4>Grace Church – Morton, IL</h4>
 				<h5>Contract: $789,000</h5><br />
 				<p>We provided all electrical work for this new church, which included a sanctuary, lobby, multiple classrooms, conference rooms, and site work. </p>
@@ -69,9 +58,10 @@
 	<article class="showcase_entry">
 		<div class="padded">
 			<div class="row-fluid">
-				<div class="showcase_entry_img span3">
+				<div class="showcase_entry_img span3 offset1">
+				<img src="http://www.placehold.it/170x120">
 				</div>
-				<div class="showcase_entry_text span9">
+				<div class="showcase_entry_text span8">
 				<h4>Additional job listings</h4>
 				<ul>
 					<li>Gourmet Express – Gridley, IL</li>
@@ -84,18 +74,12 @@
 			</div>
 		</div>
 	</article>
-	<div class="span1">
-	&nbsp;
-	</div>
 </section>
 
-<div class="span1">
-&nbsp;
-</div>
 
 <section class="row-fluid"><!--Residential-->
 	<article>
-		<header id="content_area" class="span10">
+		<header id="content_area" class="span11 offset1">
 			<div class="padded">
 				<h3>Showcase - Residential</h3>
 				<p class="body-text">Whether you’re looking for attractive exterior lighting or indoor electrical design/retrofitting for your kitchen, basement, or home theater, we provide affordable and efficient custom solutions for your home. We accept credit cards and can assist with financing, and our work is backed by a one-year conditional warranty.</p>
