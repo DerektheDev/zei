54066b10a92887e8de166163e88cc7b12210f7c1
dwysefisher <wysefisher@gmail.com>
2013-12-05 23:27:21 -0600

    showcase content added

--- staging/showcase.php
+++ staging/showcase.php
@@ -1,20 +1,26 @@
 <?php include('header.php'); ?>
-<div class="container-fluid">
 
+<div class="row-fluid">
+  <div id="mainimage" class="span12">
+    <div class="row-fluid">
+      <img src="images/autohaus350-2x.jpg" alt="Autohaus of Peoria" class="span12" />
+    </div>
+  </div>
+</div>
 <section>
 
 	<article class="row-fluid">
-		<header id="content_area" class="span11 offset1">
+		<header id="content_area" class="span10 offset1">
 			<div class="padded">
 				<h2>Showcase</h2>
-				<p class="body-text">Zeller Electric provides electrical design, installation, and maintenance for just about every industry. Our solutions fit all job sizes—and all budgets, too. For more information about our projects or services, or to receive a quote, please contact us. </p>
+				<p class="body-text">Zeller Electric provides electrical design, installation, and maintenance for just about every industry. Our solutions fit all job sizes—and all budgets, too. For more information about our projects or services, or to receive a quote, please <a href="contact.php">contact us</a>. </p>
 			</div>
 		</header>
 	</article>
 </section>
 
 <section class="row-fluid"> <!--Commercial-->
-	<header id="content_area" class="span11 offset1">
+	<header id="content_area" class="span10 offset1">
 		<div class="padded">
 			<h3>Showcase - Commercial</h3>
 			<p class="body-text">We are Central Illinois’ pre-eminent electrical contractor for outfitting commercial-grade buildings. Whether you need interior electrical design, exterior lighting, security systems, or generator installation, Zeller Electric is always ready to meet your needs&mdash;and exceed your expectations. 
@@ -26,9 +32,9 @@
 		<div class="padded">
 			<div class="row-fluid">
 				<div class="showcase_entry_img span3 offset1">
-					<img src="<?php echo $root; ?>/images/heartland.jpg" />
+					<img src="<?php echo $root; ?>/images/showcase_thumbs/heartland-showcase.jpg" alt="Heartland Community College logo" />
 				</div>
-				<div class="showcase_entry_text span8">
+				<div class="showcase_entry_text span7">
 					<h4>Heartland Community College (Phase II) – Normal, IL</h4>
 					<h5>Contract: $6,700,000</h5><br />
 					<p>This project included additions to the student center and instructional commons, new child development lab, fitness and recreation center, community education center, receiving and storage building, and site work. </p>
@@ -41,9 +47,9 @@
 		<div class="padded">
 			<div class="row-fluid">
 				<div class="showcase_entry_img span3 offset1">
-					<img src="<?php echo $root; ?>images/GraceChurch.jpg" />
+					<img src="<?php echo $root; ?>images/showcase_thumbs/GraceChurch-showcase.jpg" alt="Grace Church sign" />
 				</div>
-				<div class="showcase_entry_text span8">
+				<div class="showcase_entry_text span7">
 				<h4>Grace Church – Morton, IL</h4>
 				<h5>Contract: $789,000</h5><br />
 				<p>We provided all electrical work for this new church, which included a sanctuary, lobby, multiple classrooms, conference rooms, and site work. </p>
@@ -55,9 +61,9 @@
 		<div class="padded">
 			<div class="row-fluid">
 				<div class="showcase_entry_img span3 offset1">
-				<img src="http://www.placehold.it/170x120">
+				&nbsp;
 				</div>
-				<div class="showcase_entry_text span8">
+				<div class="showcase_entry_text span7">
 				<h4>Additional job listings</h4>
 				<ul>
 					<li>Gourmet Express – Gridley, IL</li>
@@ -76,7 +82,7 @@
 
 <section class="row-fluid"><!--Residential-->
 	<article>
-		<header id="content_area" class="span11 offset1">
+		<header id="content_area" class="span10 offset1">
 			<div class="padded">
 				<h3>Showcase - Residential</h3>
 				<p class="body-text">Whether you’re looking for attractive exterior lighting or indoor electrical design/retrofitting for your kitchen, basement, or home theater, we provide affordable and efficient custom solutions for your home. We accept credit cards and can assist with financing, and our work is backed by a one-year conditional warranty.</p>
@@ -88,9 +94,9 @@
 		<div class="padded">
 			<div class="row-fluid">
 				<div class="showcase_entry_img span3 offset1">
-					<img src="<?php echo $root; ?>http://www.placehold.it/170x120" />
+				&nbsp;
 				</div>
-				<div class="showcase_entry_text span8">
+				<div class="showcase_entry_text span7">
 				<h4>Coming Soon!</h4>
 				<h5>&nbsp;</h5><br />
 				<p>&nbsp;</p>
@@ -102,7 +108,7 @@
 
 <section class="row-fluid"><!--Industrial-->
 	<article>
-		<header id="content_area" class="span11 offset1">
+		<header id="content_area" class="span10 offset1">
 			<div class="padded">
 				<h3>Showcase - Industrial</h3>
 				<p class="body-text">Our engineering connections are key when combining innovative concepts with technical solutions, and we look forward to putting new plans into action. Whether creating motor control or electrical systems for heavy machinery or assisting in the development of programmable logic control (PLC) systems, Zeller Electric’s experienced team of experts provides a full range of design, implementation, and maintenance solutions.</p>
@@ -114,9 +120,9 @@
 		<div class="padded">
 			<div class="row-fluid">
 				<div class="showcase_entry_img span3 offset1">
-					<img src="<?php echo $root; ?>http://www.placehold.it/170x120" />
+				&nbsp;
 				</div>
-				<div class="showcase_entry_text span8">
+				<div class="showcase_entry_text span7">
 				<h4>Coming Soon!</h4>
 				<h5>&nbsp;</h5><br />
 				<p>&nbsp;</p>
@@ -128,7 +134,7 @@
 
 <section class="row-fluid"><!--Agricultural-->
 	<article>
-		<header id="content_area" class="span11 offset1">
+		<header id="content_area" class="span10 offset1">
 			<div class="padded">
 				<h3>Showcase - Agricultural</h3>
 				<p class="body-text">Zeller Electric has roots in agricultural systems, and we’ve continued to service electrical needs in this important Midwestern industry ever since. Our agricultural services include creating and implementing systems for grain bins, grain legs, and general electrical systems—all with an emphasis on efficiency and safety.</p>
@@ -140,12 +146,30 @@
 		<div class="padded">
 			<div class="row-fluid">
 				<div class="showcase_entry_img span3 offset1">
-					<img src="<?php echo $root; ?>http://www.placehold.it/170x120" />
+					<img src="<?php echo $root; ?>images/showcase_thumbs/syngentalogo-showcase.jpg" alt="Syngenta logo" />
 				</div>
-				<div class="showcase_entry_text span8">
-				<h4>Coming Soon!</h4>
-				<h5>&nbsp;</h5><br />
-				<p>&nbsp;</p>
+				<div class="showcase_entry_text span7">
+				<h4>Syngenta – Pekin, IL</h4>
+				<h5>Contract: Ongoing</h5><br />
+				<p class="body-text">We have maintained the electrical systems at Pekin’s Syngenta plant since 1979. Syngenta grows, cleans, packages, and ships hybrid seed corn and soybean seed to farmers nationwide.</p>
+				</div>
+			</div>
+		</div>
+	</article>
+	
+	<article class="showcase_entry">
+		<div class="padded">
+			<div class="row-fluid">
+				<div class="showcase_entry_img span3 offset1">
+				&nbsp;
+				</div>
+				<div class="showcase_entry_text span7">
+				<h4>Additional job listings</h4>
+				<ul>
+					<li>McKee and Roegge – Gridley, IL</li>
+					<li>Myers Crop Center: Brandt Consolidated, Inc. – Gridley, IL</li>
+					<li>Earlybird Feed and Fertilizer – Goodfield, IL</li>
+				</ul>
 				</div>
 			</div>
 		</div>
@@ -154,7 +178,7 @@
 
 <section class="row-fluid"><!--Design/Build-->
 	<article>
-		<header id="content_area" class="span11 offset1">
+		<header id="content_area" class="span10 offset1">
 			<div class="padded">
 				<h3>Showcase - Design/Build</h3>
 				<p class="body-text">Our custom solutions span a variety of projects, including sports facilities, healthcare establishments, and general commercial-grade electrical needs. When you need the entire package—concept, design, installation, and maintenance—call Zeller Electric to get your project up and running.</p>
@@ -166,21 +190,68 @@
 		<div class="padded">
 			<div class="row-fluid">
 				<div class="showcase_entry_img span3 offset1">
-					<img src="<?php echo $root; ?>http://www.placehold.it/170x120" />
+					<img src="<?php echo $root; ?>images/showcase_thumbs/fivepointslogo-showcase.jpg" alt="Five Points Washington logo" />
 				</div>
-				<div class="showcase_entry_text span8">
-				<h4>Coming Soon!</h4>
-				<h5>&nbsp;</h5><br />
-				<p>&nbsp;</p>
+				<div class="showcase_entry_text span7">
+				<h4>Five Points Washington – Washington, IL</h4>
+				<h5>Contract: $1,535,000</h5><br />
+				<p class="body-text">We designed and installed all electrical systems for this 136,000 square foot facility that houses a fitness center, indoor walking/running track, aquatics arena, two-court gymnasium, 1,000+ seat performing arts center, four banquet/meeting rooms, senior room, concession stand, and the Washington District Library.</p>
 				</div>
 			</div>
 		</div>
 	</article>
+	
+	<article class="showcase_entry">
+		<div class="padded">
+			<div class="row-fluid">
+				<div class="showcase_entry_img span3 offset1">
+					<img src="<?php echo $root; ?>images/showcase_thumbs/autohauslogo-showcase.jpg" alt="Autohaus of Peoria logo" />
+				</div>
+				<div class="showcase_entry_text span7">
+				<h4>Autohaus of Peoria – Peoria, IL</h4>
+				<h5>Contract: $868,000</h5><br />
+				<p class="body-text">We designed and installed electrical systems for this high-end import dealership, including showrooms, offices, and a service garage.</p>
+				</div>
+			</div>
+		</div>
+	</article>
+	<article class="showcase_entry">
+		<div class="padded">
+			<div class="row-fluid">
+				<div class="showcase_entry_img span3 offset1">
+					<img src="<?php echo $root; ?>images/showcase_thumbs/lighthouselogo-showcase.jpg" alt="Lighthouse Automotive logo" />
+				</div>
+				<div class="showcase_entry_text span7">
+				<h4>Lighthouse Automotive – Morton, IL</h4>
+				<h5>Contract: $342,565</h5><br />
+				<p class="body-text">Our design/build specialists reconfigured all electrical and communication systems to help keep the dealership fully operational during a remodel and expansion of the entire facility.  </p>
+				</div>
+			</div>
+		</div>
+	</article>
+	<article class="showcase_entry">
+		<div class="padded">
+			<div class="row-fluid">
+				<div class="showcase_entry_img span3 offset1">
+				&nbsp;
+				</div>
+				<div class="showcase_entry_text span7">
+				<h4>Additional job listings</h4>
+				<ul>
+					<li>Chestnut Health Systems – Bloomington, IL</li>
+					<li>Kerry Sweet Ingredients – Gridley, IL</li>
+				</ul>
+				</div>
+			</div>
+		</div>
+	</article>
+	
+
 </section>
 
 <section class="row-fluid"><!--Data Systems-->
 	<article>
-		<header id="content_area" class="span11 offset1">
+		<header id="content_area" class="span10 offset1">
 			<div class="padded">
 				<h3>Showcase - Data Systems</h3>
 				<p class="body-text">Zeller Electric’s data systems division is on top of the latest technology in data, voice, CCTV, and security systems. We are one of the few companies between Chicago and St. Louis with in-house, accredited BISCI Registered Communications Distribution Designers (RCDD). Our systems group provides consulting, design, installation, testing, troubleshooting, and certification—and we partner with industry-leading companies like Panduit, Belden, Axis, and Panasonic to provide the absolute best technology systems. Our expertise and capabilities include, but are not limited to:
@@ -199,12 +270,55 @@
 		<div class="padded">
 			<div class="row-fluid">
 				<div class="showcase_entry_img span3 offset1">
-					<img src="<?php echo $root; ?>http://www.placehold.it/170x120" />
+					<img src="<?php echo $root; ?>images/showcase_thumbs/wesleyanlogo-showcase.jpg" alt="Illinois Wesleyan logo" />
 				</div>
-				<div class="showcase_entry_text span8">
-				<h4>Coming Soon!</h4>
-				<h5>&nbsp;</h5><br />
-				<p>&nbsp;</p>
+				<div class="showcase_entry_text span7">
+				<h4>Illinois Wesleyan University – Bloomington, IL</h4>
+				<h5>State Farm Hall</h5><br />
+				<p class="body-text">We contracted all technology systems for this showcase building, including copper and fiber structured cabling, CCTV, area of rescue, clocks, access control, and professional audio-visual presentation equipment. In-house and subcontractor installations were seamlessly coordinated.</p>
+				<h5>Campus-Wide Wireless Deployment</h5><br />
+				<p class="body-text">Our team installed the cable infrastructure and wireless access points throughout campus for IWU’s wireless network upgrade and enhancement.</p>
+				<h5>Fiber Backbone</h5><br />
+				<p class="body-text">We fusion-spliced pigtail connectors to existing unterminated single mode fiber, enabling high-speed and low-loss communications between the data center and other campus locations.</p>
+				</div>
+			</div>
+		</div>
+	</article>
+	<article class="showcase_entry">
+		<div class="padded">
+			<div class="row-fluid">
+				<div class="showcase_entry_img span3 offset1">
+					<img src="<?php echo $root; ?>images/showcase_thumbs/advocatelogo-showcase.jpg" alt="Advocate Oncology logo" />
+				</div>
+				<div class="showcase_entry_text span7">
+				<h4>Advocate Oncology – Eureka, IL</h4>
+				<p class="body-text">In addition to the electrical installation, we installed the voice and data infrastructure for this clinic, which is part of the Advocate Health System.</p>
+				</div>
+			</div>
+		</div>
+	</article>
+	<article class="showcase_entry">
+		<div class="padded">
+			<div class="row-fluid">
+				<div class="showcase_entry_img span3 offset1">
+					<img src="<?php echo $root; ?>images/showcase_thumbs/calvarylogo-showcase.jpg" alt="Calvary United Methodist Church logo" />
+				</div>
+				<div class="showcase_entry_text span7">
+				<h4>Calvary United Methodist Church – Normal, IL</h4>
+				<p class="body-text">Our systems group installed data, voice, and CATV systems, as well as a backbone for CCTV, CATV, and fiber back to their existing network for an addition to the church</p>
+				</div>
+			</div>
+		</div>
+	</article>
+	<article class="showcase_entry">
+		<div class="padded">
+			<div class="row-fluid">
+				<div class="showcase_entry_img span3 offset1">
+					<img src="<?php echo $root; ?>images/showcase_thumbs/lighthouselogo-showcase.jpg" alt="Lighthouse Automotive logo" />
+				</div>
+				<div class="showcase_entry_text span7">
+				<h4>Lighthouse Automotive – Morton, IL</h4>
+				<p class="body-text">As part of our overall package on this comprehensive, multi-phase remodel, Zeller Electric seamlessly worked with the client during business hours to install new voice and data communications cabling, CCTV, digital signage displays, and music and TV systems for the customer lounge.</p>
 				</div>
 			</div>
 		</div>
